1169d854f1439057c2a064a91a90694a2e83ed1c
Wambui Ndungu <wambuin18@gmail.com>
2024-03-31 09:52:13 +0300

    Add new components and update Navbar

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,4 +1,10 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Button from "./Button";
+import Camp from "./Camp";
+import Features from "./Features";
+import GetApp from "./GetApp";
+import Hero from "./Hero";
+import Guide from "./Guide";
 
-export { Navbar, Footer };
+export { Navbar, Footer, Button, Camp, Features, GetApp, Hero, Guide};
---
+++ src/Type/index.tsx
@@ -0,0 +1,6 @@
+export interface ButtonProps {
+    type: "button" | "submit";
+    title: string;
+    icon?: string;
+    variant: "btn_dark_green"
+}
